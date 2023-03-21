e7b30f02f94a87987df8ce0b33682f92ed7820ab
Ragib Ajmal <you@example.com>
2023-03-21 21:24:25 +0000

    added scroll position

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import AnimatedComponent from "@/components/animatedcomponent";
 import TypeWriter from "../components/typewriter";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <>
       <AnimatedComponent>
